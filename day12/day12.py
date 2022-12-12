81aa0c2dbda2df5d20245e12e0700dacf7e22c9d
Francesco Georg Schmitt <s8foschm@stud.uni-saarland.de>
2022-12-12 23:21:46 +0100

    switched to a graph-based search, works on test input, terminates on real input but result is wrong :(

--- day12/day12.py
+++ day12/day12.py
@@ -3,10 +3,13 @@
 # Day 12
 # Francesco Georg Schmitt
 # https://github.com/s8foschm/aoc2022
+import time
 from enum import Enum
 from pathfinding.core.grid import Grid
-from pathfinding.finder.a_star import AStarFinder
+from pathfinding.finder.dijkstra import DijkstraFinder
 from pathfinding.core.diagonal_movement import DiagonalMovement
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
 class StepStatus(Enum):
@@ -84,6 +87,30 @@ class Heightmap:
             else:
                 return self.map[x + 1][y]
 
+    def get_neighbor_index(self, index, direction):
+        rows = len(self.map)
+        cols = len(self.map[0])
+        if direction == Direction.WEST:
+            if index % cols == 0:
+                return None
+            else:
+                return index - 1
+        elif direction == Direction.EAST:
+            if index + 1 % cols == 0:
+                return None
+            else:
+                return index + 1
+        elif direction == Direction.NORTH:
+            if index < cols:
+                return None
+            else:
+                return index - cols
+        elif direction == Direction.SOUTH:
+            if index > cols * (rows - 1):
+                return None
+            else:
+                return index + cols
+
     def get_neighbors(self, x, y):
         neighbors = []
         for direction in list(Direction):
@@ -117,14 +144,26 @@ class Heightmap:
             else:
                 if nB == nA + 1:
                     return StepStatus.FORWARD
-                elif nB == nA - 1:
+                elif nB <= nA:  # not possible here because this needs to be directional
+                    return StepStatus.FORWARD
+                elif nA == nB + 1:
                     return StepStatus.BACKWARD
                 elif nA == nB:
                     return StepStatus.SAME
                 else:
                     return StepStatus.IMPOSSIBLE
+        else:
+            return StepStatus.IMPOSSIBLE
 
-    def generate_matrix(self):
+    def moves_possible(self, x, y):
+        moves = []
+        for direction in list(Direction):
+            if self.move_possible(x, y, direction) != StepStatus.IMPOSSIBLE:
+                moves.append(direction)
+        return moves
+
+    # dead code from an unsuccessful attempt at solving this without graphs, see below
+    """def generate_matrix(self):
         matrix = []
         for (x, line) in enumerate(self.map[:-1]):
             matrix_line = []
@@ -152,12 +191,34 @@ class Heightmap:
             else:
                 matrix_line.append(1)
         matrix.append(matrix_line[:-1])
-        return matrix
+        return matrix"""
+
+    def generate_graph(self):
+        G = nx.DiGraph()
+        for (x, line) in enumerate(self.map):
+            for (y, element) in enumerate(line):
+                G.add_node(x * len(line) + y, label=element, weight=ord(element))
+        for (x, line) in enumerate(self.map):
+            for (y, element) in enumerate(line):
+                curr_index = x * len(line) + y
+                nodeA = curr_index
+                for move in self.moves_possible(x, y):
+                    nodeB = self.get_neighbor_index(curr_index, move)
+                    status = map.move_possible(x, y, move)
+                    if status == StepStatus.FORWARD:
+                        G.add_edge(curr_index, self.get_neighbor_index(curr_index, move))
+                    elif status == StepStatus.BACKWARD:
+                        G.add_edge(nodeB, nodeA)
+                    elif status == StepStatus.SAME:
+                        G.add_edge(nodeA, nodeB)
+                        G.add_edge(nodeB, nodeA)
+        return G
 
 
 def parse_file(file):
     lines = file.readlines()
     return Heightmap([line[:-1] for line in lines])
+
 
 def convert_to_matrix_format(x, y):
     new_X = x * 2
@@ -166,9 +227,32 @@ def convert_to_matrix_format(x, y):
     # if new_Y < 0: new_Y = 0
     return new_X, new_Y
 
-with (open('test_input.txt', 'r')) as file:
+
+with (open('input.txt', 'r')) as file:
+    st = time.time()
+    print("Parsing file.")
     map = parse_file(file)
-    matrix = map.generate_matrix()
+
+    print("Generating graph.")
+    gr = map.generate_graph()
+    nx.draw(gr, with_labels=True)
+    plt.show()
+    print("Generated graph with {} nodes and {} edges.".format(gr.number_of_nodes(), gr.number_of_edges()))
+    original_dict = nx.get_node_attributes(gr, "label")
+    reverse_dict = dict((new_val, new_key) for new_key, new_val in original_dict.items())
+    print("Finding path.")
+    path = nx.dijkstra_path(gr, reverse_dict.get("S"), reverse_dict.get("E"))
+    path_by_letters = [original_dict.get(val) for val in path]
+    print(path)
+    print(path_by_letters)
+    print(len(path) - 1)
+    duration = time.time() - st
+    print("Execution time: ", duration)
+    # answer 499 is too high -> path too long
+
+    # the following is dead code, remaining from the previous attempt of solving this without graphs, using a matrix
+    # representing walls instead, which won't work because here, legal moves are not symmetric
+    """matrix = map.generate_matrix()
     print(len(matrix), len(matrix[0]))
     #for line in matrix:
     #    print(line)
@@ -182,9 +266,9 @@ with (open('test_input.txt', 'r')) as file:
     print(new_start_X, new_start_Y)
     print(new_end_X, new_end_Y)
     #
-    finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
+    finder = DijkstraFinder(diagonal_movement=DiagonalMovement.never)
     path, runs = finder.find_path(start, end, grid)
     print('operations:', runs, 'path length:', len(path))
     print(grid.grid_str(path=path, start=start, end=end))
 
-    print("Actual Path Length: ", (len(path) // 2))
+    print("Actual Path Length: ", (len(path) // 2))"""
